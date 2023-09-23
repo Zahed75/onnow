ea86dbe627439380bab7d8248b4a55769a9e2ac1
“Zahed75” <zahedhasan.cs@gmail.com>
2023-09-24 04:30:34 +0600

    new file:   lib/PassdataNavigation.dart 	new file:   lib/card.dart

---
+++ lib/card.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:onnow/tabBar.dart';
+
+main(){
+  runApp(const HomeApp());
+}
+
+
+
+class HomeApp extends StatelessWidget{
+
+const HomeApp({super.key});
+
+@override
+
+Widget build (BuildContext context){
+  return MaterialApp(
+    theme:ThemeData(primarySwatch: Colors.green),
+    darkTheme: ThemeData(primarySwatch: Colors.green),
+    color:Colors.blue,
+    debugShowCheckedModeBanner: false,
+    home:const HomeActivity()
+  );
+}
+
+}
+
+
+
+class HomeActivity extends StatelessWidget{
+  const HomeActivity({super.key});
+
+    @override
+
+    Widget build (BuildContext context){
+      return Scaffold(
+          appBar:AppBar(
+            title:Text("Card Page")
+          ),
+          body:Center(
+            
+            child:Card(
+              shadowColor: Color.fromRGBO(176, 234, 205, 1),
+              color:Color.fromRGBO(33, 191, 115, 1),
+              shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+              elevation: 30,
+              child:SizedBox(
+                height:200,
+                width:200,
+                child:Center(child:Text('This is a card'),)
+                
+              )
+            )
+          )
+      );
+    
+    }
+}
