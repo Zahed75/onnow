5864e8022703e53ea1520c61488fa615d60beb19
“Zahed75” <zahedhasan.cs@gmail.com>
2023-09-24 05:22:16 +0600

    modified:   lib/card.dart 	new file:   lib/circularProgress.dart

--- lib/card.dart
+++ lib/card.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:onnow/tabBar.dart';
+
 
 main(){
   runApp(const HomeApp());
---
+++ lib/circularProgress.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+main(){
+  runApp(const HomeApp());
+}
+
+
+
+class HomeApp extends StatelessWidget{
+
+const HomeApp({super.key});
+
+@override
+
+Widget build (BuildContext context){
+  return MaterialApp(
+    theme:ThemeData(primarySwatch: Colors.green),
+    darkTheme: ThemeData(primarySwatch: Colors.green),
+    color:Colors.blue,
+    debugShowCheckedModeBanner: false,
+    home:const ProgressActivity()
+  );
+}
+}
+
+
+class ProgressActivity extends StatelessWidget{
+  const ProgressActivity({super.key});
+
+  @override
+
+  Widget build (BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title:Text("Progress check"),
+      ),
+      body:Center(
+        child:CircularProgressIndicator(
+          color:Colors.green,
+          strokeWidth: 3,
+
+        ),
+      )
+    );
+  }
+}
